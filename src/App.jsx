e46408cbc6c134c2a122c8c57d6d067135f70c51
Akash Sapkota <akashh.sapkota@gmail.com>
2021-01-09 16:29:07 +0100

    error linebreak

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,11 @@
 import React from 'react';
 import './App.css';
+import Header from './Components/Header/Header';
 
 function App() {
   return (
     <div className="App">
-      <h1>Hello world</h1>
+      <Header />
     </div>
   );
 }
