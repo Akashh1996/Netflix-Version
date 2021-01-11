e2d22907d0c0ccfd843b760cbe5bce2d718d7d46
Akash Sapkota <akashh.sapkota@gmail.com>
2021-01-11 17:19:22 +0100

    cover photo text fixed

--- src/Components/Header/Header.jsx
+++ src/Components/Header/Header.jsx
@@ -39,9 +39,17 @@ function Header({ dispatch }) {
                 <img src="https://trello-attachments.s3.amazonaws.com/5f9fe516582bea5ce01d06b2/5ffc633b4dead97ec390f11b/741e7b7ee8502992cd8e9bafc0150771/netflix.jpg" alt="logo" />
               </Link>
             </div>
-            <div className="nav-item__home">Home</div>
-            <div className="nav-item__latest">Latest</div>
-            <div className="nav-item__myList">My List</div>
+            <div className="nav-item__home">
+              {' '}
+              <Link to="/">Home</Link>
+              {' '}
+            </div>
+            <div className="nav-item__latest">
+              <Link to="/latest">Latest</Link>
+            </div>
+            <div className="nav-item__myList">
+              <Link to="/MyList">My List</Link>
+            </div>
           </div>
           <div className="nav-items-right">
             <div className="nav-search">
