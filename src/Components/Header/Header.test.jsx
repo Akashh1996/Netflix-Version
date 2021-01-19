5dd4b25f37bc00b24742c2e8168c9522fc223028
santiagorobinet <santiagorobinet@gmail.com>
2021-01-19 13:17:43 +0100

    add header test 100%

--- src/Components/Header/Header.test.jsx
+++ src/Components/Header/Header.test.jsx
@@ -2,37 +2,32 @@ import React from 'react';
 import { render, fireEvent } from '@testing-library/react';
 import { Provider } from 'react-redux';
 import { BrowserRouter } from 'react-router-dom';
-import configureStore from 'redux-mock-store';
-import thunk from 'redux-thunk';
+import configureStore from '../../redux/configureStore';
 import Header from './Header';
 import { loadBySearch } from '../../redux/actions/movieAction';
 
 jest.mock('../../redux/actions/movieAction');
 
-const buildStore = configureStore([thunk]);
-
 describe('Given The Header Component', () => {
-  beforeEach(() => {
-    const dispatch = jest.fn();
-    const initialState = {
-      movieReducer: {},
-      dispatch,
-    };
-    const store = buildStore(initialState);
+  let wrapper = null;
+  let store = null;
+
+  const wrapperFactory = (wrapperInitialState) => {
+    store = configureStore(wrapperInitialState);
     store.dispatch = jest.fn();
 
-    const Wrapper = ({ children }) => (
+    return ({ children }) => (
       <Provider store={store}>
         <BrowserRouter>
           {children}
         </BrowserRouter>
       </Provider>
     );
-    render(<Header />, { wrapper: Wrapper });
-  });
+  };
 
   afterEach(() => {
     jest.restoreAllMocks();
+    wrapper = null;
   });
 
   describe('When there is an input value in the search bar', () => {
@@ -43,11 +38,44 @@ describe('Given The Header Component', () => {
           value: 'hellboy',
         },
       };
+
+      wrapper = wrapperFactory();
+
+      render(<Header />, { wrapper });
+
       const inputsElements = document.querySelector('#input_search');
 
       fireEvent.change(inputsElements, event);
 
       expect(loadBySearch).toHaveBeenCalled();
+    });
+  });
+
+  describe('When window scrollY is > 10', () => {
+    test('Then class nav__black should exist in the document', () => {
+      wrapper = wrapperFactory();
+
+      render(<Header />, { wrapper });
+
+      fireEvent.scroll(window, { target: { scrollY: 100 } });
+
+      const navElement = document.querySelector('.nav__black');
+
+      expect(navElement).toBeInTheDocument();
+    });
+  });
+
+  describe('When window scrollY is < 10', () => {
+    test('Then class nav__black shouldn`t exist in the document', () => {
+      wrapper = wrapperFactory();
+
+      render(<Header />, { wrapper });
+
+      fireEvent.scroll(window, { target: { scrollY: 5 } });
+
+      const navElement = document.querySelector('.nav__black');
+
+      expect(navElement).not.toBeInTheDocument();
     });
   });
 });
