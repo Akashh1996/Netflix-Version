5866bb70f17429380a90754699a3a422e9b43aef
santiagorobinet <santiagorobinet@gmail.com>
2021-02-12 19:20:57 +0100

    add back basic config

---
+++ Back/index.ts
@@ -0,0 +1,17 @@
+const app = require('express')();
+const chalk = require('chalk');
+const debug = require('debug')('app*');
+const { urlencoded, json } = require('body-parser');
+const { connect } = require('mongoose');
+const morgan = require('morgan')
+
+const port = process.env.PORT || 5000
+const databaseURL = 'mongodb+srv://root:root@cluster0.s3syf.mongodb.net/Netflix?retryWrites=true&w=majority'
+
+/* connect(databaseURL, { useUnifiedTopology: true, useNewUrlParser: true }) */
+app.use(morgan('dev'))
+app.use(urlencoded({ extended: true }))
+app.use(json())
+
+
+app.listen(port, () => debug(chalk.blue.underline(`Server is running in port ${chalk.red.bgYellow.bold(port)}`)))
