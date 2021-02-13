0d2b25e7e33ded4bf0728753ba8a7c176c08aaa5
santiagorobinet <santiagorobinet@gmail.com>
2021-02-13 23:08:39 +0100

    akashiitoo

--- Back/src/controller/userController.ts
+++ Back/src/controller/userController.ts
@@ -1,13 +1,15 @@
 import { Request, Response } from 'express';
 
-interface userControllerInterface{
-    getUser: Function,
-    postUser: Function
+interface userControllerInterface {
+  getUser: Function,
+  postUser: Function
+  addToFavourites: Function
+  removeFromFAvourites: Function
 }
 
-function userController(User: any) : userControllerInterface {
-  async function getUser({ body } : Request, res: Response) {
-    const { email } = body;
+function userController(User: any): userControllerInterface {
+
+  async function getUser({ body: { email } }: Request, res: Response) {
     const query = { email };
     try {
       const user = await User.findOne(query);
@@ -16,7 +18,8 @@ function userController(User: any) : userControllerInterface {
       res.send(error);
     }
   }
-  async function postUser({ body } : Request, res: Response) {
+
+  async function postUser({ body }: Request, res: Response) {
     const { email } = body;
     try {
       const query = { email };
@@ -28,9 +31,41 @@ function userController(User: any) : userControllerInterface {
       res.send(error);
     }
   }
+
+
+  async function addToFavourites({ body }: Request, res: Response) {
+    const { email, id, image } = body
+
+    try {
+      const userFind = await User.findOne({ email });
+      userFind.favourites.push({ id, image });
+      await userFind.save()
+      res.json(userFind)
+
+    } catch (error) {
+      res.send(error)
+    }
+
+
+  }
+
+  async function removeFromFAvourites({ body }: Request, res: Response) {
+    const { email, id } = body
+    try {
+      const userFind = await User.findOne({ email });
+      const filteredFavourites = userFind.favourites.filter((favourite: favInterface) => favourite.id !== id);
+
+      userFind.favourites = filteredFavourites;
+      await userFind.save();
+      res.json(userFind)
+    } catch (error) {
+      res.send(error)
+    }
+
+  }
+
   return {
-    getUser, postUser,
+    getUser, postUser, removeFromFAvourites, addToFavourites
   };
 }
-
 export default userController;
--- Back/src/router/userRouter.ts
+++ Back/src/router/userRouter.ts
@@ -2,18 +2,15 @@ import userController from '../controller/userController';
 
 const { Router } = require('express');
 
-function userRouter(User:any) {
+function userRouter(User: any) {
   const router = Router();
   const user = userController(User);
 
   router.route('/')
-    .get(user.getUser);
-  router.route('/post')
-    .post(user.postUser);
-  /*   router.route('/addToFav')
-    .put(user.addToFavourites);
-  router.route('/removeFav')
-    .put(user.removeFromFAvourites); */
+    .get(user.getUser)
+    .post(user.postUser)
+    .put(user.addToFavourites)
+    .patch(user.removeFromFAvourites)
   return router;
 }
 
