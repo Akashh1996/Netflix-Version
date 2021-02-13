d7ad3fa4ae8f1070062cbb6f0c55718154f78d81
Akash Sapkota <akashh.sapkota@gmail.com>
2021-02-13 17:36:55 +0100

    created basic source files

--- Back/index.ts
+++ Back/index.ts
@@ -3,15 +3,16 @@ const chalk = require('chalk');
 const debug = require('debug')('app*');
 const { urlencoded, json } = require('body-parser');
 const { connect } = require('mongoose');
-const morgan = require('morgan')
+const morgan = require('morgan');
 
-const port = process.env.PORT || 5000
-const databaseURL = 'mongodb+srv://root:root@cluster0.s3syf.mongodb.net/Netflix?retryWrites=true&w=majority'
+app.use(morgan('dev'));
+app.use(urlencoded({ extended: true }));
+app.use(json());
 
-/* connect(databaseURL, { useUnifiedTopology: true, useNewUrlParser: true }) */
-app.use(morgan('dev'))
-app.use(urlencoded({ extended: true }))
-app.use(json())
+const port = process.env.PORT || 5000;
+const databaseURL = 'mongodb+srv://root:root@cluster0.s3syf.mongodb.net/Netflix?retryWrites=true&w=majority';
+connect(databaseURL, { useUnifiedTopology: true, useNewUrlParser: true });
 
-
-app.listen(port, () => debug(chalk.blue.underline(`Server is running in port ${chalk.red.bgYellow.bold(port)}`)))
+app.listen(port, () => {
+  debug(chalk.blue.underline(`Server is running in port ${chalk.red.bgYellow.bold(port)}`));
+});
