f01ebf3403d72b7525d978cdc9c56df5d19d4b9f
santiagorobinet <santiagorobinet@gmail.com>
2021-02-14 18:35:30 +0100

    move handleclick to utils

--- Front/src/Components/MyList/MyList.jsx
+++ Front/src/Components/MyList/MyList.jsx
@@ -1,17 +1,22 @@
 import React from 'react';
 import { connect } from 'react-redux';
+import { deleteFav } from '../../redux/actions/userAction';
+import './MyList.scss';
 
 function MyList({ getUserDB }) {
   const fav = getUserDB?.favourites;
   return (
-    <>
+    <div className="favourites-container">
       {fav && fav?.length > 0
         ? fav.map((user) => (
-          <img className="movie-photo" src={`https://image.tmdb.org/t/p/w500/${user.image}`} alt="user-fav" />
+          <div className="movie-photo__container">
+            <img key={user.favourites?.id} className="movie-photo" src={`https://image.tmdb.org/t/p/w500/${user.image}`} alt="user-fav" />
+            <button onClick="">BORRAR</button>
+          </div>
         )) : (
           <h1 style={{ marginTop: '80px' }}>You dont have any movies in fav list</h1>
         )}
-    </>
+    </div>
   );
 }
 
